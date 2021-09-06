9a3d0911309c47705fd62620c311f9c370393412
Jade Quinn <jadekquinn@gmail.com>
2021-09-06 16:17:46 +0000

    Update page routes in run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -7,7 +7,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("index.html")
+
+
+@app.route("/recipe")
+def about():
+    return render_template("recipe.html")
+
 
 if __name__ == "__main__":
     app.run(
